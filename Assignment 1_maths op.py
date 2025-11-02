680f308e1d4fb6a6febe41bd7a3d86d9d3d6bff6
bluesoul003 <rajt4136@gmail.com>
2025-11-03 05:06:49 +0530

    Add files via upload

---
+++ Assignment 1_maths op.py
@@ -0,0 +1,12 @@
+First_number = int(input("Enter the First Number: "))
+Second_number = int(input("Enter the Second Number: "))
+
+Addition = First_number + Second_number
+Subtraction = First_number - Second_number
+Multiplication = First_number * Second_number
+Division = First_number / Second_number
+
+print("Addition: ", Addition)
+print("Subtraction: ", Subtraction)
+print("Multiplication: ", Multiplication)
+print("Division: ", Division)
---
+++ assignment 1_greeting.py
@@ -0,0 +1,7 @@
+first_name = input("Enter your first name: ")
+last_name = input("Enter your last name: ")
+Full_name = first_name + " " + last_name
+
+Greeting = ("Hello, " +Full_name +"!" + " Welcome to the Python program.")
+
+print(Greeting)
